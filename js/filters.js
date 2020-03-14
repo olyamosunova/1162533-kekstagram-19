124f81b4e52f5b298d42ec50df5e3fe3d0eba11c
Olga <vesnushka.ol@mail.ru>
2020-03-14 23:53:57 +0200

    заканчивает фильтрацию изображений

--- js/filters.js
+++ js/filters.js
@@ -3,19 +3,45 @@
 (function () {
   var COUNT_PICTURES = 10;
 
+  var imgFilterElement = document.querySelector('.img-filters');
+  var imgFilterButtonElements = imgFilterElement.querySelectorAll('.img-filters__button');
+  var imgFiltersDefaultElement = imgFilterElement.querySelector('#filter-default');
+  var imgFiltersRandomElement = imgFilterElement.querySelector('#filter-random');
+  var imgFiltersDiscussedElement = imgFilterElement.querySelector('#filter-discussed');
+
+  var removePictures = function () {
+    var picturesContainerElement = document.querySelector('.pictures');
+    var pictureElements = picturesContainerElement.querySelectorAll('.picture');
+
+    pictureElements.forEach(function (item) {
+      picturesContainerElement.removeChild(item);
+    });
+  };
+
+  var removeActiveFilter = function () {
+    imgFilterButtonElements.forEach(function (item) {
+      if (item.classList.contains('img-filters__button--active')) {
+        item.classList.remove('img-filters__button--active');
+      }
+    });
+  };
+
   var getDefaultPicture = function (data) {
-    // return data;
-    console.log(data);
+    return data;
   };
 
   var getRandomPicture = function (data) {
     var randomPictures = [];
     for (var a = 0; a < COUNT_PICTURES; a++) {
       var numberIndex = window.utils.getRandomInteger(1, data.length);
-      randomPictures.push(data[numberIndex - 1]);
+
+      if (randomPictures.includes(data[numberIndex - 1])) {
+        a -= 1;
+      } else {
+        randomPictures.push(data[numberIndex - 1]);
+      }
     }
-    // return randomPictures;
-    console.log(randomPictures);
+    return randomPictures;
   };
 
   var getDiscussedPicture = function (data) {
@@ -23,13 +49,31 @@
       return b.comments.length - a.comments.length;
     });
 
-    // return discussedPictures;
-    console.log(discussedPictures);
+    return discussedPictures;
   };
 
-  window.filters = {
-    getDefaultPicture: getDefaultPicture,
-    getDiscussedPicture: getDiscussedPicture,
-    getRandomPicture: getRandomPicture
+  var showDefaultPicture = function (evt) {
+    removePictures();
+    removeActiveFilter();
+    window.debounce(window.pictures.renderPictures(getDefaultPicture(window.pictures.publications)));
+    evt.target.classList.add('img-filters__button--active');
   };
+
+  var showRandomPicture = function (evt) {
+    removePictures();
+    removeActiveFilter();
+    window.debounce(window.pictures.renderPictures(getRandomPicture(window.pictures.publications)));
+    evt.target.classList.add('img-filters__button--active');
+  };
+
+  var showDiscussedPicture = function (evt) {
+    removePictures();
+    removeActiveFilter();
+    window.debounce(window.pictures.renderPictures(getDiscussedPicture(window.pictures.publications)));
+    evt.target.classList.add('img-filters__button--active');
+  };
+
+  imgFiltersDefaultElement.addEventListener('click', showDefaultPicture);
+  imgFiltersRandomElement.addEventListener('click', showRandomPicture);
+  imgFiltersDiscussedElement.addEventListener('click', showDiscussedPicture);
 })();
--- js/pictures.js
+++ js/pictures.js
@@ -4,6 +4,8 @@
   var picturesContainerElement = document.querySelector('.pictures');
   var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
+  var imgFilterElement = document.querySelector('.img-filters');
+
   var createPictureElement = function (picture) {
     var pictureElement = pictureTemplate.cloneNode(true);
     pictureElement.querySelector('.picture__img').src = picture.url;
@@ -11,44 +13,6 @@
     pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
 
     return pictureElement;
-  };
-
-  var filtersPicture = function (publications) {
-    var imgFilterElement = document.querySelector('.img-filters');
-    var imgFilterButtonElements = imgFilterElement.querySelectorAll('.img-filters__button');
-    var imgFiltersDefaultElement = imgFilterElement.querySelector('#filter-default');
-    var imgFiltersRandomElement = imgFilterElement.querySelector('#filter-random');
-    var imgFiltersDiscussedElement = imgFilterElement.querySelector('#filter-discussed');
-
-    imgFilterElement.classList.remove('img-filters--inactive');
-
-    for (var b = 0; b < imgFilterButtonElements.length; b++) {
-      imgFilterButtonElements[b].addEventListener('click', function (evt) {
-
-        imgFilterButtonElements.forEach(function (item) {
-          if (item.classList.contains('img-filters__button--active')) {
-            item.classList.remove('img-filters__button--active');
-          }
-        });
-
-        switch (evt.target) {
-          case imgFiltersDefaultElement:
-            imgFiltersDefaultElement.classList.add('img-filters__button--active');
-            window.filters.getDefaultPicture(publications);
-            break;
-          case imgFiltersRandomElement:
-            imgFiltersRandomElement.classList.add('img-filters__button--active');
-            window.filters.getRandomPicture(publications);
-            break;
-          case imgFiltersDiscussedElement:
-            imgFiltersDiscussedElement.classList.add('img-filters__button--active');
-            window.filters.getDiscussedPicture(publications);
-            break;
-          default:
-            break;
-        }
-      });
-    }
   };
 
   var renderPictures = function (data) {
@@ -63,16 +27,19 @@
       fragment.appendChild(pictureElement);
     }
     picturesContainerElement.appendChild(fragment);
-    filtersPicture(pictures);
   };
 
   var onDownloadSuccess = function (data) {
+    window.pictures.publications = data.slice();
     renderPictures(data);
+    imgFilterElement.classList.remove('img-filters--inactive');
   };
 
   var onDownloadError = function (errorMessage) {};
 
   window.pictures = {
+    publications: [],
+    renderPictures: renderPictures,
     onDownloadSuccess: onDownloadSuccess,
     onDownloadError: onDownloadError
   };
