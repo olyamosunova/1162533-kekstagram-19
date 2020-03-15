c270359ac34f6374956dbdd2ebfd771c80142760
Olga <vesnushka.ol@mail.ru>
2020-03-15 20:09:58 +0200

    добавляет показ части комментариев по клику на кнопку ЕЩЕ

--- js/preview.js
+++ js/preview.js
@@ -1,6 +1,8 @@
 'use strict';
 
 (function () {
+  var COMMENTS_PART_COUNT = 5;
+
   var bodyElement = document.querySelector('body');
   var bigPictureElement = document.querySelector('.big-picture');
   var bigPictureImgBlockElement = bigPictureElement.querySelector('.big-picture__img');
@@ -10,9 +12,12 @@
   var likesCountElement = bigPictureElement.querySelector('.likes-count');
   var commentsCountElement = bigPictureElement.querySelector('.comments-count');
   var socialCaptionElement = bigPictureElement.querySelector('.social__caption');
-  var socialComentCountElement = bigPictureElement.querySelector('.social__comment-count');
+  var comentShowedCountElement = bigPictureElement.querySelector('.comments-showed-count');
+  var comentCountElement = bigPictureElement.querySelector('.comments-count');
   var commentsLoaderElement = bigPictureElement.querySelector('.comments-loader');
   var newCommentElement = bigPictureElement.querySelector('.social__footer-text');
+  var showedCommentsCount = 0;
+  var comments;
 
   var removeComments = function () {
     var socialCommentsListElements = socialCommentsElement.querySelectorAll('.social__comment');
@@ -21,21 +26,53 @@
     }
   };
 
-  var showComments = function (picture) {
+  var showComments = function () {
+    var freeCommentsCount = comments.length - showedCommentsCount;
     var fragment = document.createDocumentFragment();
+    var newCommentsPartCount = freeCommentsCount < COMMENTS_PART_COUNT ? freeCommentsCount : COMMENTS_PART_COUNT;
+    var newPartComment = comments.slice(showedCommentsCount, showedCommentsCount + newCommentsPartCount);
 
-    picture.comments.forEach(function (item) {
-      var newSocialCommentElement = document.createElement('li');
-      newSocialCommentElement.className = 'social__comment';
-      newSocialCommentElement.innerHTML = '<img class="social__picture" src=' + item.avatar + ' "alt="' + item.name + '"width="35" height="35"><p class="social__text">' + item.message + '</p>';
-      fragment.appendChild(newSocialCommentElement);
+    newPartComment.forEach(function (item) {
+      var newComment = renderComments(item);
+      fragment.appendChild(newComment);
     });
-
-    removeComments();
     socialCommentsElement.appendChild(fragment);
+
+    if (showedCommentsCount === 0) {
+      commentsLoaderElement.classList.remove('hidden');
+      commentsLoaderElement.addEventListener('click', showComments);
+    }
+
+    showedCommentsCount += newCommentsPartCount;
+    comentShowedCountElement.textContent = showedCommentsCount;
+
+    if (showedCommentsCount === comments.length) {
+      commentsLoaderElement.classList.add('hidden');
+      commentsLoaderElement.removeEventListener('click', showComments);
+    }
+  };
+
+  var renderComments = function (item) {
+    var newSocialCommentElement = document.createElement('li');
+    var newSocialImgElement = document.createElement('img');
+    newSocialCommentElement.appendChild(newSocialImgElement);
+    var newSocialText = document.createElement('p');
+    newSocialCommentElement.appendChild(newSocialText);
+
+    newSocialCommentElement.className = 'social__comment';
+    newSocialImgElement.classList.add('social__picture');
+    newSocialImgElement.src = item.avatar;
+    newSocialImgElement.alt = item.name;
+    newSocialImgElement.width = '35';
+    newSocialImgElement.height = '35';
+    newSocialText.textContent = item.message;
+
+    return newSocialCommentElement;
   };
 
   var closeBigPicture = function () {
+    showedCommentsCount = 0;
+    commentsLoaderElement.removeEventListener('click', showComments);
     bigPictureElement.classList.add('hidden');
     bodyElement.classList.remove('modal-open');
   };
@@ -51,17 +88,22 @@
   });
 
   var showPreview = function (picture) {
+    removeComments();
     bigPictureElement.classList.remove('hidden');
     newCommentElement.focus();
+
     bigPictureImgElement.src = picture.url;
     likesCountElement.textContent = picture.likes;
     commentsCountElement.textContent = picture.comments.length;
     socialCaptionElement.textContent = picture.description;
 
-    showComments(picture);
+    if (picture.comments.length > 0) {
+      commentsLoaderElement.focus();
+      comentCountElement.textContent = picture.comments.length;
+      comments = picture.comments;
+      showComments();
+    }
 
-    socialComentCountElement.classList.add('hidden');
-    commentsLoaderElement.classList.add('hidden');
     bodyElement.classList.add('modal-open');
   };
 
